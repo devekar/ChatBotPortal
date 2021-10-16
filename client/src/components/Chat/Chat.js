b6ea3d8116c9f87189f2ebbf39ea7b95c50ea0c3
Vaibhav Devekar <vaibhav.devekar@gmail.com>
2021-10-16 12:26:54 -0700

    trim more code

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -3,7 +3,6 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Divider from '@material-ui/core/Divider';
 import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -11,92 +10,101 @@ import ListItemText from '@material-ui/core/ListItemText';
 import Avatar from '@material-ui/core/Avatar';
 import Fab from '@material-ui/core/Fab';
 import SendIcon from '@material-ui/icons/Send';
-import useStyles from "./styles";
+import { styles } from "./styles";
+import { render } from 'ejs';
+import { withStyles } from "@material-ui/core/styles";
+import axios from 'axios';
 
-const Chat = () => {
-  const classes = useStyles();
+class Chat extends React.Component {
+    state = {
+        phoneUsers: []
+    }
+    
+    componentDidMount() {
+        axios.get(`/phoneusers`)
+            .then(res => {
+                const phoneUsers = res.data.data.children.map(obj => obj.data);
+                this.setState({ phoneUsers });
+            });
+    }
 
-  return (
-      <div>
-        <Grid container component={Paper} className={classes.chatSection}>
-            <Grid item xs={3} className={classes.borderRight500}>
-                <List>
-                    <ListItem button key="RemySharp">
-                        <ListItemIcon>
-                        <Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/1.jpg" />
-                        </ListItemIcon>
-                        <ListItemText primary="John Wick"></ListItemText>
-                    </ListItem>
-                </List>
-                <Divider />
-                <List>
-                    <ListItem button key="RemySharp">
-                        <ListItemIcon>
-                            <Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/1.jpg" />
-                        </ListItemIcon>
-                        <ListItemText primary="Remy Sharp">Remy Sharp</ListItemText>
-                        <ListItemText secondary="online" align="right"></ListItemText>
-                    </ListItem>
-                    <ListItem button key="Alice">
-                        <ListItemIcon>
-                            <Avatar alt="Alice" src="https://material-ui.com/static/images/avatar/3.jpg" />
-                        </ListItemIcon>
-                        <ListItemText primary="Alice">Alice</ListItemText>
-                    </ListItem>
-                    <ListItem button key="CindyBaker">
-                        <ListItemIcon>
-                            <Avatar alt="Cindy Baker" src="https://material-ui.com/static/images/avatar/2.jpg" />
-                        </ListItemIcon>
-                        <ListItemText primary="Cindy Baker">Cindy Baker</ListItemText>
-                    </ListItem>
-                </List>
-            </Grid>
-            <Grid item xs={9}>
-                <List className={classes.messageArea}>
-                    <ListItem key="1">
-                        <Grid container>
-                            <Grid item xs={12}>
-                                <ListItemText align="right" primary="Hey man, What's up ?"></ListItemText>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <ListItemText align="right" secondary="09:30"></ListItemText>
-                            </Grid>
-                        </Grid>
-                    </ListItem>
-                    <ListItem key="2">
-                        <Grid container>
-                            <Grid item xs={12}>
-                                <ListItemText align="left" primary="Hey, Iam Good! What about you ?"></ListItemText>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <ListItemText align="left" secondary="09:31"></ListItemText>
-                            </Grid>
-                        </Grid>
-                    </ListItem>
-                    <ListItem key="3">
-                        <Grid container>
-                            <Grid item xs={12}>
-                                <ListItemText align="right" primary="Cool. i am good, let's catch up!"></ListItemText>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <ListItemText align="right" secondary="10:30"></ListItemText>
-                            </Grid>
-                        </Grid>
-                    </ListItem>
-                </List>
-                <Divider />
-                <Grid container style={{padding: '20px'}}>
-                    <Grid item xs={11}>
-                        <TextField id="outlined-basic-email" label="Type Something" fullWidth />
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <div>
+                <Grid container component={Paper} className={classes.chatSection}>            
+                    <Grid item xs={3} className={classes.borderRight500}>
+
+                        <List>
+                            <ListItem button key="RemySharp">
+                                <ListItemIcon>
+                                    <Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/1.jpg" />
+                                </ListItemIcon>
+                                <ListItemText primary="Remy Sharp">Remy Sharp</ListItemText>
+                            </ListItem>
+                            <ListItem button key="Alice">
+                                <ListItemIcon>
+                                    <Avatar alt="Alice" src="https://material-ui.com/static/images/avatar/3.jpg" />
+                                </ListItemIcon>
+                                <ListItemText primary="Alice">Alice</ListItemText>
+                            </ListItem>
+                            <ListItem button key="CindyBaker">
+                                <ListItemIcon>
+                                    <Avatar alt="Cindy Baker" src="https://material-ui.com/static/images/avatar/2.jpg" />
+                                </ListItemIcon>
+                                <ListItemText primary="Cindy Baker">Cindy Baker</ListItemText>
+                            </ListItem>
+                        </List>
                     </Grid>
-                    <Grid item xs={1} align="right">
-                        <Fab color="primary" aria-label="add"><SendIcon /></Fab>
+
+                    <Grid item xs={9}>
+                        <List className={classes.messageArea}>
+                            <ListItem key="1">
+                                <Grid container>
+                                    <Grid item xs={12}>
+                                        <ListItemText align="right" primary="Hey man, What's up ?"></ListItemText>
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <ListItemText align="right" secondary="09:30"></ListItemText>
+                                    </Grid>
+                                </Grid>
+                            </ListItem>
+                            <ListItem key="2">
+                                <Grid container>
+                                    <Grid item xs={12}>
+                                        <ListItemText align="left" primary="Hey, Iam Good! What about you ?"></ListItemText>
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <ListItemText align="left" secondary="09:31"></ListItemText>
+                                    </Grid>
+                                </Grid>
+                            </ListItem>
+                            <ListItem key="3">
+                                <Grid container>
+                                    <Grid item xs={12}>
+                                        <ListItemText align="right" primary="Cool. i am good, let's catch up!"></ListItemText>
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <ListItemText align="right" secondary="10:30"></ListItemText>
+                                    </Grid>
+                                </Grid>
+                            </ListItem>
+                        </List>
+                        <Divider />
+                        <Grid container style={{padding: '20px'}}>
+                            <Grid item xs={11}>
+                                <TextField id="outlined-basic-email" label="Send custom reply" fullWidth />
+                            </Grid>
+                            <Grid item xs={1} align="right">
+                                <Fab color="primary" aria-label="add"><SendIcon /></Fab>
+                            </Grid>
+                        </Grid>
                     </Grid>
                 </Grid>
-            </Grid>
-        </Grid>
-      </div>
-  );
+            </div>
+        );
+    }
 }
 
-export default Chat;
+export default withStyles(styles, { withTheme: true })(Chat);
